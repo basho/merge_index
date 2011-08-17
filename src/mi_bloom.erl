22a71ea57087f65b813bfc491754592061ba29a8
Rusty Klophaus <rklophaus@gmail.com>
2011-08-17 15:06:32 -0400

    Add license information.

--- src/mi_bloom.erl
+++ src/mi_bloom.erl
@@ -1,3 +1,25 @@
+%% -------------------------------------------------------------------
+%%
+%% mi_bloom: bloom filter adapted for use by merge index.
+%%
+%% Copyright (c) 2007-2011 Basho Technologies, Inc.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
 %% @doc Implementation of the Bloom filter data structure.
 %% @reference [http://en.wikipedia.org/wiki/Bloom_filter]
 
