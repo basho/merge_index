d2607c5805a8dd2d797d283913949f4ba9e37569
Sean Cribbs <sean@basho.com>
2011-08-30 13:08:28 -0400

    Log when a truncated/corrupted buffer is detected.

--- src/mi_buffer.erl
+++ src/mi_buffer.erl
@@ -57,17 +57,17 @@ new(Filename) ->
 
     %% Read into an ets table...
     Table = ets:new(buffer, [duplicate_bag, public]),
-    open_inner(FH, Table),
+    open_inner(FH, Table, Filename),
     {ok, Size} = file:position(FH, cur),
 
     %% Return the buffer.
     #buffer { filename=Filename, handle=FH, table=Table, size=Size }.
 
-open_inner(FH, Table) ->
-    case read_value(FH) of
+open_inner(FH, Table, Filename) ->
+    case read_value(FH, Filename) of
         {ok, Postings} ->
             write_to_ets(Table, Postings),
-            open_inner(FH, Table);
+            open_inner(FH, Table, Filename);
         eof ->
             ok
     end.
@@ -145,7 +145,7 @@ iterate_list([H|T]) ->
 %% Internal functions
 %% ===================================================================
 
-read_value(FH) ->
+read_value(FH, Filename) ->
     {ok, CurrPos} = file:position(FH, cur),
     case file:read(FH, 4) of
         {ok, <<Size:32/unsigned-integer>>} when Size > 0->
@@ -153,16 +153,21 @@ read_value(FH) ->
                 {ok, <<B:Size/binary>>} ->
                     {ok, binary_to_term(B)};
                 _ ->
+                    log_truncation(Filename, CurrPos),
                     file:position(FH, {bof, CurrPos}),
                     eof
             end;
         {ok, _Binary}  ->
+            log_truncation(Filename, CurrPos),
             file:position(FH, {bof, CurrPos}),
             eof;
         eof ->
             eof
     end.
 
+log_truncation(Filename, Position) ->
+    error_logger:info_msg("Corrupted posting detected in ~s after reading ~w bytes, ignoring remainder.",
+                          [Filename, Position]).
 
 write_to_file(FH, Terms) when is_list(Terms) ->
     %% Convert all values to binaries, count the bytes.
